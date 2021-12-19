31be2f2abeacd4cd4a2e90982d28607637445fcc
Christian Barran <48329035+justbarran@users.noreply.github.com>
2021-12-19 19:45:50 -0400

    Update Musical_Lights_Controller_V2.ino

--- Musical_Lights_Controller_V2/Musical_Lights_Controller_V2.ino
+++ Musical_Lights_Controller_V2/Musical_Lights_Controller_V2.ino
@@ -1,5 +1,5 @@
 /*
- * Muscial Lights Controller
+ * Muscial Lights Controller V2
  * Just Barran 2021
  * project video At
  * www.youtube.com/c/justbarran
@@ -7,7 +7,6 @@
  */
 
 #include "LedControl.h"
-
 
 #define msg7RESET 13
 #define msg7Strobe 9
@@ -22,6 +21,7 @@
 #define led2 3
 #define led3 4
 #define led4 5
+
 #define led5 6
 #define led6 7
 #define led7 8
@@ -31,31 +31,16 @@
 #define LED_LOW LOW  //Switch the output depening on Relays
 #define LED_HIGH HIGH  
 
-
-/*
- Now we need a LedControl to work with.
- ***** These pin numbers will probably not work with your hardware *****
- pin 12 is connected to the DataIn 
- pin 11 is connected to the CLK 
- pin 10 is connected to LOAD 
- We have only a single MAX72XX.
- */
+#define CYCLE_DELAY 50 // Time lights remain on for  
+ 
 LedControl lc=LedControl(maxDataIn,maxCLK,maxLoad,1);
 
 /* we always wait a bit between updates of the display */
 unsigned long delaytime=100;
+byte ledPins[7]= {led1,led2,led3,led4,led5,led6,led7};
 byte level[8]=   {B10000000,B11000000,B11100000,B11110000,B11111000,B11111100,B11111110,B11111111};
 byte setlevel[8]={B10000000,B01000000,B00100000,B00010000,B00001000,B00000100,B00000010,B00000001};
-byte value[7]={0,0,0,0,0,0,0};
-
-unsigned long LOWstime=0;
-unsigned long MIDstime=0;
-unsigned long HIGHstime=0;
-
-byte LOWsFlag = 0;
-byte MIDsFlag = 0;
-byte HIGHsFlag = 0;
-
+unsigned int valueMSG[7];
 unsigned int potValue=0;
 byte potSet = 0;
 
@@ -97,11 +82,11 @@ void loop()
 { 
   digitalWrite(msg7Strobe, HIGH);  
   digitalWrite(msg7RESET, HIGH);
-  potValue = analogRead(TRIM_POT);
-  potSet = map(potValue, 0, 1024, 0, 8);  
-  lc.setRow(0,7,setlevel[potSet]);
-  
   delayMicroseconds(1); //Reset Pluse Width 100nS Min
+  potValue = analogRead(TRIM_POT);
+  potSet = map(potValue, 0, 1023, 0, 8);  
+  lc.setRow(0,7,setlevel[potSet]);  
+  
   digitalWrite(msg7RESET, LOW);
   delayMicroseconds(100); // Reset to Stobe Delay 72uS min
   
@@ -111,21 +96,21 @@ void loop()
       delayMicroseconds(40); //Output settling time 36us Min
       //Read MSG Value
       int spectrumRead = analogRead(msg7DCout);
-      value[x] = map(spectrumRead, 0, 1024, 0, 8);
-      lc.setRow(0,x,level[value[x]]);
+      valueMSG[x] = map(spectrumRead, 0, 1023, 0, 8);
+      lc.setRow(0,x,level[valueMSG[x]]);
       digitalWrite(msg7Strobe, HIGH);
       delayMicroseconds(40); //Strobe to Strobe 72uS Min
       //Set Lights 
-      byte ledSet = x+2;
-      if(value[x]>=potSet)
+      if(valueMSG[x]>=potSet)
       {
-        digitalWrite(ledSet, LED_HIGH);
+        digitalWrite(ledPins[x], LED_HIGH);
       }
       else
       {
-        digitalWrite(ledSet, LED_LOW);
+        digitalWrite(ledPins[x], LED_LOW);
       }
     }
+    delay(CYCLE_DELAY); //Allow them to be on or off for a bit of time 
 }
 
 void test()
